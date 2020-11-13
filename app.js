40a02782040e882a3ab179d5b75f3322f561bff6
Yewon-july <kellychung730@gmail.com>
2020-11-13 13:27:39 +0900

    commit-again

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((err, req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const handleListening = () => 
     console.log(`Listening on: ${PORT}`);
